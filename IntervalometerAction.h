6422a3453af9c490a6221eaacd00d83135c6f336
Andrew Duncan <arduncan@bigpond.com>
2014-06-08 22:30:43 +1000

    Updated to use changes to LcdRowGraph library.

--- IntervalometerAction.h
+++ IntervalometerAction.h
@@ -2,7 +2,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2013 Andrew Duncan
+// Copyright (c) 2014 Andrew Duncan
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the
@@ -101,7 +101,7 @@ private:
     uint8_t _backlightPin;
     uint8_t _backlightValue;
     LiquidCrystal& _lcd;
-    LcdRowGraph _lcdRowGraph;
+    LcdRowGraph<LiquidCrystal> _lcdRowGraph;
     FocusShootDelayAction _focusShootDelayAction;
 };
 
